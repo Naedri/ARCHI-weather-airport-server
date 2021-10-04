6c5be75d9309c47bba7ed2637e81011782cafe3f
Adrien Jallais <adrien.jallais@protonmail.com>
2021-10-04 11:20:49 +0200

    feat: retryer

--- internal/utils/retryer.go
+++ internal/utils/retryer.go
@@ -1,52 +1,39 @@
 package utils
 
-import (
-	"time"
-)
-
-type RetryEvent struct {
-	time    time.Time
-	success bool
-	fail    error
-}
+import "time"
 
 type Retryer struct {
-	retryTimes    int
 	maxRetryTimes int
 	tickInterval  time.Duration
 	timeout       time.Duration
 	tick          func()
-	actionHandler func()
+	retryAction   func() (bool, error)
 	errorHandler  func()
 }
 
-func (r Retryer) something() {
-	//r.MaxRetryTimes
-}
-
-// will start the
-func tick(r *Retryer) {
-	//TODO
-}
-
-func newRetryer(maxRetryTimes int, tickInterval time.Duration, timeout time.Duration, actionHandler func(), errorHandler func()) (Retryer, error) {
+func newRetryer(maxRetryTimes int, tickInterval time.Duration, timeout time.Duration, retryAction func() (bool, error), errorHandler func()) *Retryer {
 	retryer := new(Retryer)
 	retryer.maxRetryTimes = maxRetryTimes
 	retryer.tickInterval = tickInterval
 	retryer.timeout = timeout
-	retryer.actionHandler = actionHandler
+	retryer.retryAction = retryAction
 	retryer.errorHandler = errorHandler
-	retryer.tick = tickRetryer
-	retryer.tick()
-
-	return *retryer, nil
+	retryer.tick = func() {
+		for i := 0; i < maxRetryTimes; i++ {
+			time.Sleep(tickInterval)
+			state, err := retryAction()
+			if err != nil {
+				errorHandler()
+			}
+			if state {
+				return
+			}
+		}
+	}
+	return retryer
 }
 
-func newDefaultRetryer(actionHandler func(), errorHandler func()) (Retryer, error) {
-	// 12 fois max, 10 seconde d'intervale, timeOut de 8 secondes
+func newDefaultRetryer(actionHandler func() (bool, error), errorHandler func()) *Retryer {
+	// 12 fois max, 10 secondes d'intervale, timeOut de 8 secondes
 	return newRetryer(12, 10*time.Second, 8*time.Second, actionHandler, errorHandler)
-}
-
-func tickRetryer() {
-
 }
